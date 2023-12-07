ae437e829d9f55c0f6bdf9fd31b7e34f648216e6
rlobz <rlobzowski@gmail.com>
2023-12-07 12:06:46 +0000

    questions for logo

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const { Triangle, Circle, Square } = require('./lib/shapes');
+
+inquirer
+.prompt([
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Enter up to three characters for the logo:',
+        validate: input => input.length <= 3
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter the text color (keyword or hexadecimal):'
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape:',
+        choices: ['circle', 'triangle', 'square']
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter the shape color (keyword or hexadecimal):'
+    }
+]);
