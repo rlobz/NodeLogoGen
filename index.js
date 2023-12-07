f41cddca1013864214c4a95e285b20d0762972f7
rlobz <rlobzowski@gmail.com>
2023-12-07 12:46:39 +0000

    finished code with save each logo not replace

--- index.js
+++ index.js
@@ -26,4 +26,26 @@ inquirer
         name: 'shapeColor',
         message: 'Enter the shape color (keyword or hexadecimal):'
     }
-]);
+])
+
+.then(answers => {
+    const safeName = answers.text.replace(/[^a-z0-9]/gi, '_').toLowerCase();
+    const filename = `logo-${safeName}.svg`;
+    const shapeMap = {
+        circle: new Circle(answers.shapeColor),
+        triangle: new Triangle(answers.shapeColor),
+        square: new Square(answers.shapeColor)
+    };
+
+    const svgContent = `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        ${shapeMap[answers.shape].render()}
+        <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="${answers.textColor}" font-size="40">${answers.text}</text>
+    </svg>`;
+
+    fs.writeFileSync(`./examples/${filename}`, svgContent);
+    console.log(`Generated ${filename}`);
+})
+
+.catch(error => {
+    console.error('An error occurred:', error);
+});
